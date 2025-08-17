7a1ac0579029c87d895c18106d3502c9350a8e2a
cocobakehouses-stack <cocobakehouses@gmail.com>
2025-08-17 19:52:27 +0700

    Create products.ts

---
+++ app/data/products.ts
@@ -0,0 +1,27 @@
+// data/products.ts
+export type Product = { 
+  id: number; 
+  name: string; 
+  price: number 
+};
+
+export const products: Product[] = [
+  { id: 1,  name: 'Matcha Macadamia',               price: 169 },
+  { id: 2,  name: 'Croissant Cookie',               price: 159 },
+  { id: 3,  name: 'Pistachio Dark Choc',            price: 159 },
+  { id: 4,  name: 'New York Signature',             price: 135 },
+  { id: 5,  name: 'Hazelnut Choc Chip',             price: 135 },
+  { id: 6,  name: 'Dark Choc 70%',                  price: 135 },
+  { id: 7,  name: 'Dark Cranberry',                 price: 135 },
+  { id: 8,  name: 'Dark Orange',                    price: 135 },
+  { id: 9,  name: 'Biscoff',                        price: 135 },
+  { id: 10, name: 'Peanut Butter',                  price: 135 },
+  { id: 11, name: 'DOUBLE Choc',                    price: 135 },
+  { id: 12, name: 'White Choc & Dark Choc',         price: 135 },
+  { id: 13, name: 'Choc Chip',                      price: 129 },
+  { id: 14, name: 'Walnut',                         price: 129 },
+  { id: 15, name: 'Red Velvet',                     price: 125 },
+  { id: 16, name: 'Lemon',                          price: 125 },
+  { id: 17, name: "S'more",                         price: 109 },
+  { id: 18, name: 'Salt Caramel',                   price: 109 },
+];
